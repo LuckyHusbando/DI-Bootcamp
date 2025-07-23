003fcbc7b7925d6589dd5455e29ad49097d13902
Emma <emma@gmail.com>
2025-07-23 11:12:06 +0300

    Updating Exercises

---
+++ Week1/Day2/XP/exercise.py
@@ -0,0 +1,39 @@
+#Exercise 1
+print(("Hello world \n")*4)
+
+#Exercise 2
+print(int(99*99*99)*8)
+
+#Exercise 3
+False
+True
+False
+False
+False
+
+#Exercise 4
+
+computer_brand = "Lenovo"
+print(f"I have a {str(computer_brand)} computer.")
+
+#Exercise 5
+
+name = "Derek"
+age = 41
+shoe_size = 44
+info = "My name is Derek, I am 41 years old, and wear 44 size shoes. How strange!?"
+print(info)
+
+#Exercise 6 A & B
+
+A = 256
+B = 1024
+if B > A:
+    print("Hello world")
+
+#Exercise 7
+
+user_name = input("Please insert a number.")
+print(user_name)
+
+If
