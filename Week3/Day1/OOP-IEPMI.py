271dc34cc9f60d3e545fa955164a1cbb889185da
Emma <emma@gmail.com>
2025-08-03 18:43:08 +0300

    Updating

---
+++ Week3/Day1/OOP-IEPMI.py
@@ -0,0 +1,16 @@
+# Object Oriented Programming - Inheritance, Encapsulation, Polymorphism, and Multiple Inheritance
+
+# Inheritance & Advantages of Inheritance
+
+
+
+# Polymorphism
+
+
+
+# Encapsulation
+
+
+
+# Multiple Inheritance
+
