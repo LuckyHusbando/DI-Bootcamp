665c6ddb6dc079fb31b8297800e439dfdb5f319c
Emma <emma@gmail.com>
2025-08-12 19:33:02 +0300

    Exercises

---
+++ Week4/Day2/AI-Agentic-Presentation.py
@@ -0,0 +1,48 @@
+#Alain Prasquier's Presentation - AI Agentic - How is it entering the business space? - 12/08/2025
+
+#What is an AI Agent?
+
+#3 principles - Autonomous, Perception, Reasoning
+
+#Independent behavior and judgment, reasoning ability, able to perceive information presented to it and make judgments and actions.
+
+#Example bots: Customer Support, Sales Outreach, Research Assistant, Code review agent, Travel organizer, HR onboard coach, (etc)
+
+#Creating agents is actually very easy - ChatGPT 5.0 now has the ability to have an agent do a task in the background while a human user works.
+
+#Anything with intensive administrative tasks can now be done by sophisticated AI. 
+
+#General belief at present: 15-50% of business tasks will be performed by AI agents. 
+
+#95% of Generative AI projects never reach production!!!
+
+#Disruptive Innovation with AI:
+#1. No oversight!
+#2. No business stakeholders (at present)
+#3. Distruption -instead- of productivity
+#4. Significant Risk exposure - The "non-deterministic" conundrum. 
+
+#Most tasks & processes - 'IF' > 'THEN' > 'ELSE'
+
+#Mission - Enable business stakeholders to operate mission-critical AI agents with confidence.
+#Vision - Power the Future of Work for hyper-productivity. 
+
+#AI-Agent Tooling marketing is not yet mature
+
+#Path to AI Agent Adoption
+
+#1. Custom-built Agents (Mission Critical - Core/proprietary processes, custom workflows, private/confidential/regulated activities)
+#2. AaaS (Agents as a Service) (Off-the-shelf AI Agent) (Standardize workflows of all aspects of business)
+#3. AI Extensions (enhancing existing software)
+
+#"Fleet" Management System for AI Agents
+#Human Users of a service, service is run by several different AI agents fed on data of professional business practices in different sectors. 
+
+#www.supervaize.com (Alain's business - bring an AI agent in the hands of users)
+
+#1. Get business users onboard.
+#2. Have developers be comfortable using the platform.
+
+#1. https://github.com/supervaize/supervaizer
+#2. Create an account (https://stage.supervaize.com/)
+#3. Discount Code - DI202508 (Free Subscription)
