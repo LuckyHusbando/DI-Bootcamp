824b08c4a210ab02ca49a59bf497235ab22221f2
Emma <emma@gmail.com>
2025-08-12 14:07:34 +0300

    Updates

---
+++ Week4/Day1/DailyChallenge/Exercise1-Quizz.py
@@ -0,0 +1,23 @@
+# What is a class?
+#A blueprint for creating objects. I.E. a template that defines attributes and methods.
+
+# What is an instance?
+#An instance is an individual object created from a class. So if a class is a blueprint for making houses, an instance would be a house.
+
+# What is encapsulation?
+#It is the bundling of data (attributes) and methods (functions) that operate on the data in a single unit, which is Class. 
+
+# What is abstraction?
+#The process of hiding complex implementation details and showing only essential features. It focuses on 'what' over 'how'. Used to simplify tasks for any given user.
+
+# What is inheritance?
+#A process that allows a new class (a child class) to adopt attributes & methods from an existing class (or parent class).
+
+# What is multiple inheritance?
+#This is when a class inherits attributes & methods from several different classes (2 or more).
+
+# What is polymorphism?
+#A concept where many different classes can be treated as if they were objects of a common base class. It allows calls for data to be answered in a unique way for each instance.
+
+# What is method resolution order or MRO?
+#This is the sequence which Python looks for methods in a hierarchy of classes, especially when inheriting from multiple classes. It is the order at which processes are used inside complex systems.
